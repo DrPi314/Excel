c06d82d405b3784f7a987536f54d966b7a314451
DrPi314 <48885229+DrPi314@users.noreply.github.com>
2019-03-24 11:16:54 -0500

    Add files via upload

--- RouteBuilding2.py
+++ RouteBuilding2.py
@@ -14,17 +14,28 @@ hr = 9
 num_columns = 6
 stops_t = []
 routes_t = []
+cols = [4, 2, 3, 1]
 
 
 def numrows():
     num_rows = 0
-    for row_index in range(9, cs.nrows):
-        cell_val = cs.cell(row_index, 1)
+    for row_index in range(hr, cs.nrows):
+        cell_val = cs.cell(row_index, 1).value
         if cell_val != '':
-            print(cell_val, 'filled')
             num_rows += 1
         else:
             break
     return(num_rows)
     
-print(numrows())
+
+def stops():
+    for row_index in range(hr, numrows()):
+        for col_index in cols:
+            cell_val = cs.cell(row_index, col_index).value
+            if cs.cell_type(row_index, col_index) == xlrd.XL_CELL_DATE:
+                my_time = int(cell_val*24)
+                stops_t.append(my_time)
+            else:
+                stops_t.append(cell_val)
+                
+print(stops())
