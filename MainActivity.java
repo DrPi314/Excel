d9e83d1453e5fcb6c5b180fd47333f5752f1ab7a
DrPi314 <48885229+DrPi314@users.noreply.github.com>
2019-04-11 17:07:49 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,81 +1,123 @@
-package com.example.contractortracker;
+package com.example.bds;
 
-import android.content.Intent;
+import android.Manifest;
+import android.inputmethodservice.Keyboard;
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import jxl.*;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.Cell;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.biff.CellValue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button equipment;
-    public Button time;
-    public Button meals;
-    public Button mileage;
-    public Button fuel;
-    public Button maintenance;
-    public Button settings;
+    public Button btnStart;
+    public Button btnEnd;
+    public Button btnTaxStart;
+    public Button btnTaxEnd;
+    EditText eDate;
+    EditText eTime;
+    EditText eOdometer;
+    EditText eWaitTime;
+    EditText eNumberStops;
+    private static final String FILE_PATH = "E:\\OneDrive\\AppAccess\\Pagels Books 2019.xlsx";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        equipment = (Button) findViewById(R.id.btnEquip);
-        time = (Button) findViewById(R.id.btnTime);
-        meals = (Button) findViewById(R.id.btnMeal);
-        mileage = (Button) findViewById(R.id.btnMiles);
-        fuel = (Button) findViewById(R.id.btnFuel);
-        maintenance = (Button) findViewById(R.id.btnMaint);
-        settings = (Button) findViewById(R.id.btnSettings);
+        btnStart = findViewById(R.id.btnStart);
+        btnEnd = findViewById(R.id.btnEnd);
+        btnTaxStart = findViewById(R.id.btnTaxStart);
+        btnTaxEnd = findViewById(R.id.btnTaxEnd);
+        eDate = findViewById(R.id.eDate);
+        eTime = findViewById(R.id.eTime);
+        eOdometer = findViewById(R.id.eOdometer);
+        eWaitTime = findViewById(R.id.eWaitTime);
+        eNumberStops = findViewById(R.id.eNumberStops);
 
-        equipment.setOnClickListener(new View.OnClickListener() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Equipment.class));
+                Start();
+            }
+        });
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                End();
+            }
+        });
+        btnTaxStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaxStart();
+            }
+        });
+        btnTaxEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaxEnd();
             }
         });
 
-        time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Time.class));
-            }
-        });
-
-        meals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Meals.class));
-            }
-        });
-
-        mileage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Mileage.class));
-            }
-        });
-
-        fuel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Fuel.class));
-            }
-        });
-
-        maintenance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Maintenance.class));
-            }
-        });
-
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Settings.class));
-            }
-        });
     }
+
+    private void Start() {
+    }
+
+    private void End() {
+    }
+
+    private void TaxStart() {
+
+    }
+
+    private void TaxEnd() {
+        String time = eTime.getText().toString();   //col 2
+        String mile = eOdometer.getText().toString();  //col 4
+        Workbook wb;
+        Integer col;
+        Integer row;
+        try
+        {
+            wb = Workbook.getWorkbook(new File(FILE_PATH));
+            Sheet s = wb.getSheet(0);
+
+            for (int row=1; row < s.getRows(); row++) //worried about this loop setup, maybe getrows+1?
+                if (s.getCell(2, row) == null) {
+
+                } else {
+                    continue;
+                }
+        }
+        catch (BiffException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        eTime.setText("");
+        eOdometer.setText("");
+
+    }
+
+
 }
